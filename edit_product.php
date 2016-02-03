0675eea69a3ed4bd0fadf2f6d53d88fa71c06132
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-03 16:15:01 +0530

    Update edit_product.php

--- edit_product.php
+++ edit_product.php
@@ -1,4 +1,4 @@
-<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
 <?php	require __DIR__.'/style.css'; ?>
 <h1>EDIT PRODUCT</h1>
 <a href="index.php">Back</a>
@@ -48,7 +48,7 @@
 <input type="submit" name="submit" value="Save"/>
 </form>
 <script>
-jQuery(function() {
+jQuery(window).load(function(){
 	jQuery("#add_new_country").click(function() {
 		var eid = parseInt(jQuery('.upsell_product_id').length+1);
 			jQuery("div[class^=addnew]:last").after('<label>Country</label><select name="country'+eid+'"><option value="arizona">Arizona</option><option value="atlanta">Atlanta</option><option value="baltimore">Baltimore</option><option value="buffalo">Buffalo</option><option value="carolina">Carolina</option><option value="chicago">Chicago</option><option value="cincinnati">Cincinnati</option><option value="cleveland">Cleveland</option><option value="dallas">Dallas</option><option value="denver">Denver</option><option value="detroit">Detroit</option><option value="green_bay">Green Bay</option><option value="indianapolis">Indianapolis</option><option value="jacksonville">Jacksonville</option><option value="kansas_city">Kansas City</option><option value="miami">Miami</option><option value="minnesota">Minnesota</option><option value="new_england">New England</option><option value="new_orleans">New Orleans</option><option value="new_york">New York</option><option value="oakland">Oakland</option><option value="philadelphia">Philadelphia</option><option value="pittsburgh">Pittsburgh</option><option value="san_diego">San Diego</option><option value="san_francisco">San Francisco</option><option value="seattle">Seattle</option><option value="washington">Washington</option></select><br/><label>Upsell Product IDs(Seprated by ",")</label><textarea name="upsell_product_id'+eid+'" class="upsell_product_id"></textarea><br/><div class="addnew"></div>');
@@ -56,4 +56,14 @@ jQuery(function() {
 		jQuery('#n_pages').val(parseInt(n_page)+parseInt(1));
 	});
 });
+</script>
+<script>
+/* jQuery(function() {
+	jQuery("#add_new_country").click(function() {
+		var eid = parseInt(jQuery('.upsell_product_id').length+1);
+			jQuery("div[class^=addnew]:last").after('<label>Country</label><select name="country'+eid+'"><option value="arizona">Arizona</option><option value="atlanta">Atlanta</option><option value="baltimore">Baltimore</option><option value="buffalo">Buffalo</option><option value="carolina">Carolina</option><option value="chicago">Chicago</option><option value="cincinnati">Cincinnati</option><option value="cleveland">Cleveland</option><option value="dallas">Dallas</option><option value="denver">Denver</option><option value="detroit">Detroit</option><option value="green_bay">Green Bay</option><option value="indianapolis">Indianapolis</option><option value="jacksonville">Jacksonville</option><option value="kansas_city">Kansas City</option><option value="miami">Miami</option><option value="minnesota">Minnesota</option><option value="new_england">New England</option><option value="new_orleans">New Orleans</option><option value="new_york">New York</option><option value="oakland">Oakland</option><option value="philadelphia">Philadelphia</option><option value="pittsburgh">Pittsburgh</option><option value="san_diego">San Diego</option><option value="san_francisco">San Francisco</option><option value="seattle">Seattle</option><option value="washington">Washington</option></select><br/><label>Upsell Product IDs(Seprated by ",")</label><textarea name="upsell_product_id'+eid+'" class="upsell_product_id"></textarea><br/><div class="addnew"></div>');
+			var n_page=jQuery('#n_pages').val();
+		jQuery('#n_pages').val(parseInt(n_page)+parseInt(1));
+	});
+}); */
 </script>
