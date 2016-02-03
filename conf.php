06a2043a57b6918fb21edb9b66d7808a45f7bc3d
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-03 17:26:50 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -2,3 +2,12 @@
 
 	define('SHOPIFY_APP_API_KEY', '7662e5daf152854818a5066e4d5e61d6');
 	define('SHOPIFY_APP_SHARED_SECRET', '77e2b001dfdb274bada219d669a0528d');
+	$host = "host=ec2-54-225-199-245.compute-1.amazonaws.com";
+	$port = "port=5432";
+	$dbname = "dbname=dbddrf8jsndbge";
+	$credentials = "user=sousqigydxxmjh password=KJBXNEMK8Vcyyf5FgIJK6yfygO";
+	$db = pg_connect( "$host $port $dbname $credentials"  );
+	if(!$db){
+	    echo "Error : Unable to open database\n";
+	}
+?>
