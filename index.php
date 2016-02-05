d324098caa3bd53e3923f6f855e4acd8e5e5ad7f
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-05 15:58:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
 					 pg_query($db,"UPDATE app_shop_data SET access_token =  '$oauth_token'  WHERE shop_url = '".$_GET['shop']."'");
 			 }
 		else{
+			$sql = "UPDATE app_shop_data SET access_token =  '$oauth_token'  WHERE shop_url = '".$_GET['shop']."'";
 			pg_query($db,"UPDATE app_shop_data SET access_token =  '$oauth_token'  WHERE shop_url = '".$_GET['shop']."'"); 
 			/* echo "INSERT INTO app_shop_data (access_token,shop_url) VALUES ('".$_GET['shop']."','".$oauth_token."')"; */
 			/* pg_query($db,"INSERT INTO app_shop_data (access_token,shop_url) VALUES ('".$_GET['shop']."','".$oauth_token."')");  */
