2d34d2634d488eb9037de5b80c39d80a6fe29079
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-05 17:02:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,11 +18,7 @@
 		  $result = pg_query($db,"SELECT * from app_shop_data where shop_url='".$_GET['shop']."'"); 
 	
 			if(pg_num_rows($result) > 0){
-				pg_query($db,"UPDATE app_shop_data SET access_token =  '$oauth_token'  WHERE shop_url = '".$_GET['shop']."'"); 
-			 }
-		else{	pg_query($db,"INSERT INTO app_shop_data (access_token,shop_url) VALUES ('".$oauth_token."','".$_GET['shop']."')");
-		
-			pg_query($db,"CREATE TABLE IF NOT EXISTS ".$_GET['shop']."product(
+				$sql= "CREATE TABLE IF NOT EXISTS ".$_GET['shop']."product(
   pid serial NOT NULL,
   shop_id serial NOT NULL,
   product_id character varying(500),
@@ -30,7 +26,21 @@
   country character varying(100),
   upsell_product character varying(100)
   CONSTRAINT productkey PRIMARY KEY (pid)
-)");
+)"
+			pg_query($db,$sql);
+				pg_query($db,"UPDATE app_shop_data SET access_token =  '$oauth_token'  WHERE shop_url = '".$_GET['shop']."'"); 
+			 }
+		else{	pg_query($db,"INSERT INTO app_shop_data (access_token,shop_url) VALUES ('".$oauth_token."','".$_GET['shop']."')");
+		$sql= "CREATE TABLE IF NOT EXISTS ".$_GET['shop']."product(
+  pid serial NOT NULL,
+  shop_id serial NOT NULL,
+  product_id character varying(500),
+  upsell_show character varying(100),
+  country character varying(100),
+  upsell_product character varying(100)
+  CONSTRAINT productkey PRIMARY KEY (pid)
+)"
+			pg_query($db,$sql);
 			
 				
 		} 
