77e70b67883d0609686fd8d640dede3906ea3a18
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-05 15:30:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 	$credentials = "user=sousqigydxxmjh password=KJBXNEMK8Vcyyf5FgIJK6yfygO";
 	$db = pg_connect( "$host $port $dbname $credentials"  );
 		  $result = pg_query($db,"SELECT * from app_shop_data "); //where shop_url=".$_GET['shop']check if the store exists
-			if($select_store->pg_num_rows($result) > 0){
+			if(pg_num_rows($result) > 0){
 			 echo "dfdsd";
 					  /*  $sql = "UPDATE app_shop_data SET access_token =  $oauth_token  WHERE shop_url = $_GET['shop']";
 					 pg_query($db,"UPDATE app_shop_data SET access_token =  $oauth_token  WHERE shop_url = $_GET['shop']"); */
