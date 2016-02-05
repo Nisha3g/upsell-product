0400170f43313455ad0d8bc8de3386a33433c24c
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-05 16:57:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 	 $oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
 	 $_SESSION['auth_token']=$oauth_token;
 	 $_SESSION['shop_url']=$_GET['shop'];
-	  //create new db class
-    /*     $db = new DB(); */
 	$host = "host=ec2-54-225-199-245.compute-1.amazonaws.com";
 	$port = "port=5432";
 	$dbname = "dbname=dbddrf8jsndbge";
@@ -17,21 +15,29 @@
 	$password="KJBXNEMK8Vcyyf5FgIJK6yfygO";
 	$credentials = "user=sousqigydxxmjh password=KJBXNEMK8Vcyyf5FgIJK6yfygO";
 	$db = pg_connect( "$host $port $dbname $credentials"  );
-/* 	echo "SELECT * from app_shop_data where shop_url='".$_GET['shop']."'"; */
 		  $result = pg_query($db,"SELECT * from app_shop_data where shop_url='".$_GET['shop']."'"); 
-	 /*   echo pg_num_rows($result); */
+	
 			if(pg_num_rows($result) > 0){
 				pg_query($db,"UPDATE app_shop_data SET access_token =  '$oauth_token'  WHERE shop_url = '".$_GET['shop']."'"); 
 			 }
-		else{	
-			pg_query($db,"INSERT INTO app_shop_data (access_token,shop_url) VALUES ('".$oauth_token."','".$_GET['shop']."')");  
+		else{	pg_query($db,"INSERT INTO app_shop_data (access_token,shop_url) VALUES ('".$oauth_token."','".$_GET['shop']."')");
+		
+			pg_query($db,"CREATE TABLE IF NOT EXISTS ".$_GET['shop']."product(
+  pid serial NOT NULL,
+  shop_id serial NOT NULL,
+  product_id character varying(500),
+  upsell_show character varying(100),
+  country character varying(100),
+  upsell_product character varying(100)
+  CONSTRAINT productkey PRIMARY KEY (pid)
+)");
+			
+				
 		} 
 	}
 	 $shopify = shopify\client($_SESSION['shop_url'], SHOPIFY_APP_API_KEY,$_SESSION['auth_token']);
    	try
-	{
-		//echo "<script>alert(1)</script>";
-		# Making an API request can throw an exception
+	{		
 		$products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
 		?>
 		<div class="maintable">
