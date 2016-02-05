f6118f4dd8d6d857896505ee7cf1a2e9b9cd6168
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-05 17:34:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 		  $result = pg_query($db,"SELECT * from app_shop_data where shop_url='".$_GET['shop']."'"); 
 	
 			if(pg_num_rows($result) > 0){
-			echo	$sql= "CREATE TABLE IF NOT EXISTS product_".$_GET['signature']."(
+			echo	$sql= "CREATE TABLE IF NOT EXISTS product_".$oauth_token."(
   pid serial NOT NULL,
   shop_id serial NOT NULL,
   product_id character varying(500),
@@ -31,7 +31,7 @@
 				pg_query($db,"UPDATE app_shop_data SET access_token =  '$oauth_token'  WHERE shop_url = '".$_GET['shop']."'"); 
 			 }
 		else{	pg_query($db,"INSERT INTO app_shop_data (access_token,shop_url) VALUES ('".$oauth_token."','".$_GET['shop']."')");
-	echo	$sql= "CREATE TABLE IF NOT EXISTS  product_".$_GET['signature']."(
+	echo	$sql= "CREATE TABLE IF NOT EXISTS  product_".$oauth_token."(
   pid serial NOT NULL,
   shop_id serial NOT NULL,
   product_id character varying(500),
