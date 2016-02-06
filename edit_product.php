ba76ac1f1bce90b021f6b050e50f7a7764d2cbbf
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-06 14:56:23 +0530

    Update edit_product.php

--- edit_product.php
+++ edit_product.php
@@ -126,7 +126,7 @@ $result = pg_query($db,"SELECT * from product_".$_SESSION['auth_token']." where 
 </form>
 <script>
 
-	$("#add_new_country").click(function() {alert(1);
+	$("#add_new_country").click(function() {
 		 var eid = parseInt($('.upsell_product_id').length+1);
 			$("div[class^=addnew]:last").after('<label>Country</label><select name="country'+eid+'"><option value="arizona">Arizona</option><option value="atlanta">Atlanta</option><option value="baltimore">Baltimore</option><option value="buffalo">Buffalo</option><option value="carolina">Carolina</option><option value="chicago">Chicago</option><option value="cincinnati">Cincinnati</option><option value="cleveland">Cleveland</option><option value="dallas">Dallas</option><option value="denver">Denver</option><option value="detroit">Detroit</option><option value="green_bay">Green Bay</option><option value="indianapolis">Indianapolis</option><option value="jacksonville">Jacksonville</option><option value="kansas_city">Kansas City</option><option value="miami">Miami</option><option value="minnesota">Minnesota</option><option value="new_england">New England</option><option value="new_orleans">New Orleans</option><option value="new_york">New York</option><option value="oakland">Oakland</option><option value="philadelphia">Philadelphia</option><option value="pittsburgh">Pittsburgh</option><option value="san_diego">San Diego</option><option value="san_francisco">San Francisco</option><option value="seattle">Seattle</option><option value="washington">Washington</option></select><br/><label>Upsell Product IDs(Seprated by ",")</label><textarea name="upsell_product_id'+eid+'" class="upsell_product_id"></textarea><br/><div class="addnew"></div>');
 			var n_country=$('#n_country').val();
