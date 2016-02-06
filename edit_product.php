6dad8f45f2d9056f2b9ef15bce9c9d4f7f9eced4
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-06 12:14:22 +0530

    Update edit_product.php

--- edit_product.php
+++ edit_product.php
@@ -8,9 +8,9 @@ if(isset($_REQUEST['submit'])){
 		echo $country  ='country'.$loop;
 		echo $upsell_product_id  ='upsell_product_id'.$loop;
 		echo "SELECT * from product_".$oauth_token." where product_id='".$_REQUEST['id']."' and country='".$_REQUEST[$country]."'";
-		echo "INSERT INTO product_".$oauth_token." (shop_id,product_id,upsell_show,country,upsell_product) VALUES ('".$_SESSION['shop_id'];."','".$_REQUEST['id']."','".$_REQUEST['upsell_show']."','".$_REQUEST[$country]."','".$_REQUEST[
+		/*		echo "INSERT INTO product_".$oauth_token." (shop_id,product_id,upsell_show,country,upsell_product) VALUES ('".$_SESSION['shop_id'];."','".$_REQUEST['id']."','".$_REQUEST['upsell_show']."','".$_REQUEST[$country]."','".$_REQUEST[
 			$upsell_product_id]."')";
-	/*		 $result = pg_query($db,"SELECT * from product_".$oauth_token." where product_id='".$_REQUEST['id']."' and country='".$_REQUEST[$country]."'");		
+	 $result = pg_query($db,"SELECT * from product_".$oauth_token." where product_id='".$_REQUEST['id']."' and country='".$_REQUEST[$country]."'");		
 		if(pg_num_rows($result) > 0){
 			
 		}
