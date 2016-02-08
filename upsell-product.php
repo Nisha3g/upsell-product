6231c1b2d6954d3bd615170b973e750fb5bd6920
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-08 11:59:13 +0530

    Update upsell-product.php

--- upsell-product.php
+++ upsell-product.php
@@ -1,22 +1,34 @@
 <html>
 	<head>
 		<title></title>
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 	</head>
 	<body class="vaping2">
-<?php session_start();
+<script>
+$.ajax( { 
+  url: 'http://freegeoip.net/json/', 
+  type: 'POST', 
+  dataType: 'jsonp',
+  success: function(location) {
+	   jQuery('#country-name').html(location.country_name);
+  }
+} );
+</script>
+<?php 
+session_start();
 print_r($_SESSION);
 require __DIR__.'/conf.php'; 
 require __DIR__.'/style.css';
-echo  "SELECT * from product_".$_REQUEST['access_token']." where product_id='".$_REQUEST['product_id']."' and upsell_show='0'";
-			$result = pg_query($db,"SELECT * from product_".$_REQUEST['access_token']." where product_id='".$_REQUEST['product_id']."' and upsell_show='0'");
-				if(pg_num_rows($result) > 0){
-					while($row= pg_fetch_array($result)){
-						print_r($row);
-					}						
-					
-					
-				}
-			?>		
+echo  "SELECT * from product_".$_REQUEST['access_token']." where product_id='".$_REQUEST['product_id']."' and upsell_show='0'"; 
+?>
+<div id="country-name" name="country-name"/>
+<?php
+$result = pg_query($db,"SELECT * from product_".$_REQUEST['access_token']." where product_id='".$_REQUEST['product_id']."' and upsell_show='0'");
+	if(pg_num_rows($result) > 0){
+		while($row= pg_fetch_array($result)){
+			print_r($row);			
+		}
+	}
+?>		
 	</body>
 </html>
