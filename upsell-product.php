92dd0198b667e696a75065e8dd8638ebef001a2e
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-08 16:33:53 +0530

    Update upsell-product.php

--- upsell-product.php
+++ upsell-product.php
@@ -7,9 +7,9 @@ $product_id = $_REQUEST['product_id'];
 $result = pg_query($db,"SELECT * from product_{$token} where product_id='{$product_id}' and upsell_show= '0' and country='{$country}'");
 if(pg_num_rows($result) > 0){
 	while($row= pg_fetch_array($result)){
-		print_r($row);
+		$upsell_products=$row['upsell_product'];
 	}
 } ?>
-document.write("<div id='cartrelatedproduct' style='display:none'><a class='fancybox-close1' href='/cart'></a><div class='popup'><div class='content'>");
+document.write("<div id='cartrelatedproduct' style='display:none'><a class='fancybox-close1' href='/cart'></a><div class='popup'><div class='content'><?php echo $upsell_products; ?></div></div></div>");
 
 
