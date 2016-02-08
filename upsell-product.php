66dadc971890676ef3309dc89acba58c24fe2029
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-08 12:54:11 +0530

    Update upsell-product.php

--- upsell-product.php
+++ upsell-product.php
@@ -4,34 +4,9 @@
 		<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 	</head>
 	<body class="vaping2">
-<?php/* <script>
-$.ajax( { 
-  url: 'https://freegeoip.net/json/', 
-  type: 'POST', 
-  dataType: 'jsonp',
-  success: function(location) {
-	  alert(location.country_name);
-	   jQuery('#country-name').html(location.country_name);
-	   
-  }
-} );
-</script> */?>
- <?php
-/* echo $user_ip = $_SERVER['REMOTE_ADDR'];
-$geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
-echo $country = $geo["geoplugin_countryName"];
-echo $city = $geo["geoplugin_city"]; */
-/* $ip = $_SERVER['REMOTE_ADDR'];
-$add="http://ipinfo.io/".$ip."/json";
-$add="http://ipinfo.io/10.159.155.105/json";
-$details = json_decode(file_get_contents($add));
-print_R($details);
-echo "<br/>"; */
+<?php session_start();
  $location = file_get_contents('http://freegeoip.net/json/'.$_SERVER['HTTP_X_FORWARDED_FOR']);
- print_r($location);
- print_r($_SERVER);
-session_start();
-print_r($_SESSION);
+echo  $country_name=$location->country_name;
 require __DIR__.'/conf.php'; 
 require __DIR__.'/style.css';
 echo  "SELECT * from product_".$_REQUEST['access_token']." where product_id='".$_REQUEST['product_id']."' and upsell_show='0'"; 
