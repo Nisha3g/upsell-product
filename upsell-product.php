457df45aebfd713760cb441f040ee4fc2907625a
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-08 13:12:38 +0530

    Update upsell-product.php

--- upsell-product.php
+++ upsell-product.php
@@ -10,11 +10,11 @@
  $country=$location['country_name'];
 require __DIR__.'/conf.php'; 
 require __DIR__.'/style.css';
-echo  "SELECT * from product_".$_REQUEST['access_token']." where product_id='".$_REQUEST['product_id']."' and upsell_show='0'"; 
+echo  "SELECT * from product_".$_REQUEST['access_token']." where product_id='".$_REQUEST['product_id']."' and upsell_show='0' and country='".$country."'"; 
 ?>
 <div id="country-name" name="country-name"/>
 <?php
-$result = pg_query($db,"SELECT * from product_".$_REQUEST['access_token']." where product_id='".$_REQUEST['product_id']."' and upsell_show='0'");
+$result = pg_query($db,"SELECT * from product_".$_REQUEST['access_token']." where product_id='".$_REQUEST['product_id']."' and upsell_show='0' and country='".$country."'");
 	if(pg_num_rows($result) > 0){
 		while($row= pg_fetch_array($result)){
 			print_r($row);			
