78b2fa923fc9cbfbaf6ae22bfafbeaea8000b49d
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-08 16:29:14 +0530

    Update upsell-product.php

--- upsell-product.php
+++ upsell-product.php
@@ -1,1 +1,15 @@
-document.write('<?php $location = json_decode(file_get_contents('http://freegeoip.net/json/'.$_SERVER['HTTP_X_FORWARDED_FOR']),true);$country=$location['country_name'];require __DIR__.'/conf.php';?><div id="country-name" name="country-name"/><?php $result = pg_query($db,"SELECT * from product_".$_REQUEST['access_token']." where product_id='".$_REQUEST['product_id']."' and upsell_show='0' and country='".$country."'");if(pg_num_rows($result) > 0){while($row= pg_fetch_array($result)){print_r($row);}}?><div id="cartrelatedproduct" style="display:none"><a class="fancybox-close1" href="/cart"></a> <div class="popup"><div class="content">');
+<?php 
+$location = json_decode(file_get_contents('http://freegeoip.net/json/'.$_SERVER['HTTP_X_FORWARDED_FOR']),true);
+$country = $location['country_name'];
+require __DIR__.'/conf.php'; 
+$token = $_REQUEST['access_token'];
+$product_id = $_REQUEST['product_id'];
+$result = pg_query($db,"SELECT * from product_{$token} where product_id='{$product_id}' and upsell_show= '0' and country='{$country}'");
+if(pg_num_rows($result) > 0){
+	while($row= pg_fetch_array($result)){
+		print_r($row);
+	}
+} ?>
+document.write("<div id='cartrelatedproduct' style='display:none'><a class='fancybox-close1' href='/cart'></a><div class='popup'><div class='content'>");
+
+
