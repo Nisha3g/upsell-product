8cdf189755cdb5c8b69fa12243dd88b207ab7cf6
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-09 17:12:06 +0530

    Update edit_product.php

--- edit_product.php
+++ edit_product.php
@@ -34,8 +34,8 @@ if(pg_num_rows($result) > 0){while($row= pg_fetch_array($result)){
 <?php $product_id=$_GET['id'];
  $shopify = shopify\client($_SESSION['shop_url'], SHOPIFY_APP_API_KEY,$_SESSION['auth_token']);
 $products = $shopify("GET /admin/products/{$product_id}.json", array('published_status'=>'published'));
-/* print_r($shopify);
-print_r($products); */
+/* print_r($shopify);*/
+print_r($products); 
 ?>
 <h1>EDIT PRODUCT</h1>
 <a href="index.php">Back</a>
