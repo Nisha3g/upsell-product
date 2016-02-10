483c6d048c6225a184150e11774599e58f45dd10
Nisha3g <nisha.3ginfo@gmail.com>
2016-02-10 17:28:12 +0530

    Update upsell-product.php

--- upsell-product.php
+++ upsell-product.php
@@ -31,11 +31,46 @@ if(pg_num_rows($result) > 0){
  echo $up_p_id=$upsell_product[$i];
  $products = $shopify("GET /admin/products/{$upsell_product[$i]}.json", array('published_status'=>'published'));
  ?> 
- document.write("<div class='item '><div class='countdiv' style='display:none'></div><div class='product-wrapper product-<?php echo $products['id']; ?>'><div class='listviewcontent'><div class='product-image'><img src='' alt='{{ product.title | escape  }}' /></div>     		<div class='product-name'><?php echo $products['title']; ?></div></div>      	<div class='listviewcontent2'>         <div class='product-partnum'>{{ product.variants.first.sku }}</div>  <div class='product-buy'>      <form method='post' action=''>      <div class='product-price regular' style='display:none'>$19.99</div>      <div class='product-price sale' style=''>{{ product.price | money }}</div>      <div class='product-buttons' id='900162372-3542601220'>      <input name='quantity' style='display:none' type='text' value='1' maxlength='5' class='qty'>      <input id='addtocart1' name='addtocart' type='button' value='Add to Cart' class='addtocart-<?php echo $products['id']; ?> addtocart-<?php echo $products['handle']; ?> addtocart-{{ product.metafields.scarcity.producttype  }}'  onclick='savecart1('{{ product.variants.first.id }}','<?php echo $products['id']; ?>','{{ product.metafields.upsell-colname.upsell-colname }}','{{ item.product.metafields.upsell-qty.upsell-qty }}');'>        </div></form></div>   		</div>        <div class='cleardiv'></div></div>     </div>");
+ document.write("<div class='item '><div class='countdiv' style='display:none'></div><div class='product-wrapper product-<?php echo $products['id']; ?>'><div class='listviewcontent'><div class='product-image'><img src='' alt='{{ product.title | escape  }}' /></div>     		<div class='product-name'><?php echo $products['title']; ?></div></div>      	<div class='listviewcontent2'>         <div class='product-partnum'>{{ product.variants.first.sku }}</div>  <div class='product-buy'>      <form method='post' action=''>      <div class='product-price regular' style='display:none'>$19.99</div>      <div class='product-price sale' style=''>{{ product.price | money }}</div>      <div class='product-buttons' id='900162372-3542601220'>      <input name='quantity' style='display:none' type='text' value='1' maxlength='5' class='qty'>      <input id='addtocart1' name='addtocart' type='button' value='Add to Cart' class='addtocart-<?php echo $products['id']; ?> addtocart-<?php echo $products['handle']; ?> addtocart-{{ product.metafields.scarcity.producttype  }}'  onclick='savecart1('{{ product.variants.first.id }}','<?php echo $products['id']; ?>','{{ product.metafields.upsell-colname.upsell-colname }}','{{ item.product.metafields.upsell-qty.upsell-qty }}');'>        </div></form></div>   		</div>        <div class='cleardiv'></div></div>      </div> ");
 
   
 <?php } ?>
-
+ document.write("</div></div><div style='clear:both'></div>
+      <div id='button1' class='popupbottom1' style='float:right'><style>#upsellclosebtn{float: none; color:#000;text-decoration:underline;margin-right:10px;}#upsellclosebtn:hover{float: none;color:#000;text-decoration:none;margin-right:10px;}</style><a id='upsellclosebtn' href='/checkout' class='upsell_no_thanks'>No Thanks</a><a id='inline' href='/checkout' class='product-modal pm1 cart btn' style='color: rgb(255, 255, 255); margin-bottom: 7px; margin-right: 25px; margin-top: 7px; background-color: rgb(48, 194, 117);float:left'>Checkout</a></div>");
+  $(document).ready(function() {
+    if(window.location.search == "?up=1"){
+      var axes2 = new Array();
+      var i=0;
+      {% for item in cart.items %}
+         if("{{ item.product.metafields.upsell-type.upsell-type }}") {
+           axes2[i]="{{ item.product.metafields.upsell-type.upsell-type }}";
+           i++;
+         }
+      {% endfor %}
+      console.log(axes2);
+      
+      if(axes2.length != 0){
+        $.fancybox.open([
+        {
+          href : '#cartrelatedproduct',
+          //title : '1st title'
+        }
+        ], {
+          padding : 0   
+        });
+      }
+    }
+  });
+   $('.product-modal').fancybox({
+    helpers: {
+      overlay: {
+        locked: false
+      }
+    },
+  'afterClose':function () {
+      window.location.href='/checkout';
+    }
+  });
 
 
 
